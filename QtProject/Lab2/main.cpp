f466e5bdb89712c7c2d27f2d092c0a0d14d0aa8c
Isaac Laryea <62596396+kobbyrythm@users.noreply.github.com>
2020-04-14 17:12:14 +0200

    Update main.cpp

--- QtProject/Lab2/main.cpp
+++ QtProject/Lab2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 
 #include "problem1.h"
@@ -7,6 +8,72 @@
 
 int main()
 {
-    std::cout << "Good luck for this second lab" << std::endl;
+    std::cout << "Question 1\n";
+
+  //CEASAR CIPHER
+ std::string text;
+  int s;
+	std::cout << "Enter ciphertext and press enter to continue." <<std::endl;
+  std::getline(std::cin,text);
+  std::cout << "Enter desired shift" <<std::endl;
+	std::cin>> s;
+	std::cout << "Text : " << text;
+	std::cout << "\nShift: " << s;
+	std::cout << "\nCipher: " << encrypt(text, s);
+
+
+
+
+//VIGENERE CIPHER
+
+  std::string input;
+  std::string keyword;
+	std::cout << "Enter ciphertext:";
+  std::getline(std::cin,input);
+
+  std::cout << "Enter keyword:";
+  std::getline(std::cin,keyword);
+
+	std::string key = generateKey(input, keyword);
+	std::string cipher_text = cipherText(input, key);
+
+	std::cout << "Ciphertext : "
+		<< cipher_text << "\n";
+
+
+//PROBLEM 2
+//CEASAR DECRYPTION CIPHER
+std::cout << "Question 2\n";
+
+std::string text;
+int s;
+std::cout << "Enter encryptedtext and press enter to continue." <<std::endl;
+std::getline(std::cin,text);
+std::cout << "Enter desired shift" <<std::endl;
+std::cin>> s;
+std::cout << "Text : " << text;
+std::cout << "\nShift: " << s;
+std::cout << "\nCipher: " << encrypt(text, 26-s);
+
+
+
+//VIGENERE DECRYPTION
+std::string input;
+std::string keyword;
+std::cout << "Enter ciphertext:";
+std::getline(std::cin,input);
+
+std::cout << "Enter keyword:";
+std::getline(std::cin,keyword);
+
+std::string key = generateKey(input, keyword);
+std::string cipher_text = cipherText(input, key);
+
+std::cout << "Ciphertext : "
+  << cipher_text << "\n";
+
+      std::cout << "Decrypted Text : "
+           << originalText(cipher_text, key);
+
     return 0;
 }
