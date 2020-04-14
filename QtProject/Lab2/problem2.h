887221619a641e8cbd45127285afa2164f494702
Isaac Laryea <62596396+kobbyrythm@users.noreply.github.com>
2020-04-14 17:16:25 +0200

    Update problem2.h

--- QtProject/Lab2/problem2.h
+++ QtProject/Lab2/problem2.h
@@ -1,6 +1,27 @@
 #ifndef PROBLEM2_H
 #define PROBLEM2_H
 namespace problem2 {
+  
 
+std::string encrypt(std::string text, int s)
+{
+	std::string result = "";
 }
+
+
+std::string generateKey(std::string input, std::string key)
+{
+	int x = input.size();
+}
+
+
+std::string cipherText(std::string input, std::string key)
+{
+	std::string cipher_text;
+}
+
+std::string originalText(std::string cipher_text, std::string key)
+{
+    std::string orig_text;
+}}
 #endif // PROBLEM2_H
